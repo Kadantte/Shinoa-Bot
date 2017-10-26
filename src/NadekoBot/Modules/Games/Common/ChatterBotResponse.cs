2cfe8a6cb7d9dc70412495a53d1953fe27e86871
Gremagol <xgremagolx@gmail.com>
2017-10-26 11:12:25 +0200

    Hello Ene 2.0 :heart:

--- src/NadekoBot/Modules/Games/Common/ChatterBotResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NadekoBot.Modules.Games.Common
-{
-    public class ChatterBotResponse
-    {
-        public string Convo_id { get; set; }
-        public string BotSay { get; set; }
-    }
-}
